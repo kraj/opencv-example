129bb09f48761b35279de859049d33b83b8035c4
Khem Raj <raj.khem@gmail.com>
2023-04-09 08:25:28 -0700

    init

---
+++ DisplayImage.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+std::string version = "1.0";
+int main (int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        std::cout << "DisplayImage: " << version << std::endl;
+        std::cout << "usage: DisplayImage </path/to/Image>" << std::endl;
+        return -1;
+    }
+    Mat image;
+    image = imread( argv[1], IMREAD_COLOR );
+    if ( !image.data )
+    {
+        std::cout << "No image data" << std::endl;
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+}
